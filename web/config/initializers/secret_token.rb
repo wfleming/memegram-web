ff80abaae99d59de68b14525e1fd81e6314b9ba4
Will Fleming <will@mintdigital.com>
2011-11-14 16:21:11 -0500

    rails setup

--- web/config/initializers/secret_token.rb
+++ web/config/initializers/secret_token.rb
@@ -4,4 +4,4 @@
 # If you change this key, all old signed cookies will become invalid!
 # Make sure the secret is at least 30 characters and all random,
 # no regular words or you'll be exposed to dictionary attacks.
-Web::Application.config.secret_token = '996ec431df8084f54b22c13e2e10cadec894a58492d6b4f55925a6040acc78e3ef384578ae5d9485a58c59b7d87afb79d3592dcb410000ba92045ddd9f11d455'
+Memogram::Application.config.secret_token = 'f344222fd4f7bc8baafdcc8d3daafd42bf1f89f5effc35fdac4a7591f00b9c605a35ff9a43ea83f37b7e105d8bc8de2d389677295f88b8f244ae4c4eab5d814a'
