c311bca141715345492ccc5ba5ec57d9d569bacb
Will Fleming <will@mintdigital.com>
2011-11-14 15:29:40 -0500

    first commit

---
+++ web/config/initializers/airbrake.rb
@@ -0,0 +1,3 @@
+Airbrake.configure do |config|
+  config.api_key = '910868358df722bc465af33bf346d098'
+end
---
+++ web/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Web::Application.config.secret_token = '996ec431df8084f54b22c13e2e10cadec894a58492d6b4f55925a6040acc78e3ef384578ae5d9485a58c59b7d87afb79d3592dcb410000ba92045ddd9f11d455'
