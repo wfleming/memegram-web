4782a18fa8aee3a3117d0ada63dfcf7e8e16dd11
Will Fleming <will@jwock.org>
2011-11-19 20:17:01 -0500

    let's try it this way

--- config/initializers/s3.rb
+++ config/initializers/s3.rb
@@ -1,4 +1,8 @@
-AWS::S3::Base.establish_connection!(
-  :access_key_id     => Configuration::S3.key,
-  :secret_access_key => Configuration::S3.secret
-)
+begin
+  AWS::S3::Base.establish_connection!(
+    :access_key_id     => Configuration::S3.key,
+    :secret_access_key => Configuration::S3.secret
+  )
+rescue Exception => e
+  Rails.logger.warn("SERIOUS LIKELIHOOD OF BADNESS: we were not able to connect to S3. Details: #{e}")
+end
